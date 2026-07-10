240110ee7bc4d2801eac4048ca4e4f1efdb712f0
Gabriel Herter <hertergabriel4@gmail.com>
2026-07-10 20:58:22 -0300

    Deploy: mobile UX retest#2 pass (scroll, mindmap, voice dictation, crew tap-assign, jobs diagram)

--- assets/chunk-55IACEB6-C0bcxiLm.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./mermaid.core-B8nst4eY.js";import{m as o}from"./client-W5Zd4zrx.js";var d=a((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
