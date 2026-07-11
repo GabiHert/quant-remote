284c00f1c06a852143483879eb754110b8050679
Gabriel Herter <hertergabriel4@gmail.com>
2026-07-11 19:25:54 -0300

    mobile: tab bar hugs the bottom edge (near-flush safe-area guard)

---
+++ assets/chunk-55IACEB6-B5GhMJSN.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./mermaid.core-BbwSKAkW.js";import{m as o}from"./client-_4T_z4-w.js";var d=a((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
