1fb3e1a577f47a33e60a7a60914d91d711c4bb74
Gabriel Herter <hertergabriel4@gmail.com>
2026-07-12 02:32:31 -0300

    mobile: hide the tab bar while the keyboard is open

---
+++ assets/chunk-QZHKN3VN-JQh24e6I.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./mermaid.core-Irk2BwZS.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
