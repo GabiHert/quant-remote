f03c7a19e1164cc99814f3e0cd23de7387dfa959
Gabriel Herter <hertergabriel4@gmail.com>
2026-07-11 23:12:43 -0300

    temp: gap diag v3 — runtime viewport-height experiment

--- assets/chunk-55IACEB6-CHkw9fID.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./mermaid.core-BaBr9zL2.js";import{m as o}from"./client-CSWKuv7V.js";var d=a((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
