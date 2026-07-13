ef4b948618390e93d9cb1528b490b1163bc2b3b1
Gabriel Herter <hertergabriel4@gmail.com>
2026-07-13 01:56:34 -0300

    mobile: Shift key toggle, PWA install prompt (Android + iOS coach), Voice replaces Jobs in nav bar

--- assets/chunk-55IACEB6-DLmlrPLe.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./mermaid.core-D6maOU0O.js";import{m as o}from"./client-DbabR8p_.js";var d=a((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
